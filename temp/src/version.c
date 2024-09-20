369d64162f42ade6331d7044afa7e963deff83a2
rayandu924 <rayandu924@gmail.com>
2024-09-20 07:31:58 +0000

    Refactor directory structure and add missing files

---
+++ temp/src/version.c
@@ -0,0 +1,6 @@
+#include "version.h"
+const char *janus_build_git_sha = "504daf5aef333d6f37e41c30b00be24cfb6c83bf";
+const char *janus_build_git_time = "mer. 18 sept. 2024 06:38:50 UTC";
+int janus_version = 1300;
+const char *janus_version_string = "1.3.0";
+const char *libnice_version_string = "0.1.21";
